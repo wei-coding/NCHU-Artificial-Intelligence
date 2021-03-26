616bca3f526935ff7cf5d6167831c37c8940a57c
wei-coding <edward.yu0830@gmail.com>
2021-03-26 23:35:06 +0800

    成功版，會更新路徑了

--- astar/main.py
+++ astar/main.py
@@ -49,64 +49,95 @@ class StartNode(Node):
     H = 0
 
 
+class PathNotFoundError(Exception):
+    pass
+
+
 def a_star(start_pos, end_pos, map_: list):
-    open_list = []
+    open_list = [start_pos]
     close_list = []
     """start search"""
-    close_list.append(start_pos)
-    next_node = start_pos
-    while next_node != end_pos:
-        _search_neighbor(map_, next_node, open_list, close_list)
-        for n in open_list:
-            print(n.POS, n.F, end=' ')
-        print()
-        next_node = _next_step(next_node, end_pos, open_list, close_list)
-        print(next_node.POS)
-        end_pos.FROM = next_node
-    previous_node = end_pos.FROM
-    route = []
-    while previous_node is not None:
-        route.append(previous_node.POS)
-        previous_node = previous_node.FROM
+    start_pos.G = 0
+    start_pos.H = _distance(start_pos, end_pos)
+    start_pos.F = start_pos.H
+
+    while len(open_list) != 0:
+        x = lowest_f_in(open_list)
+        if x == end_pos:
+            print('Found path!')
+            return construct_route(end_pos)
+        open_list.remove(x)
+        close_list.append(x)
+        for y in neighbor_of(x, map_):
+            if y in close_list:
+                continue
+            guess_g = x.G + _real_distance(x, y)
+            if y not in open_list:
+                guess_better = True
+            elif guess_g < y.G:
+                guess_better = True
+            else:
+                guess_better = False
+            if guess_better:
+                print(f'from {x.POS} to {y.POS}')
+                y.FROM = x
+                y.G = guess_g
+                y.H = _distance(y, end_pos)
+                y.F = y.G + y.H
+                open_list.append(y)
+            if y == end_pos:
+                end_pos = y
+    raise PathNotFoundError()
+
+
+def construct_route(end_pos):
+    route = [end_pos]
+    previous = end_pos.FROM
+    while previous is not None:
+        route.append(previous)
+        previous = previous.FROM
     route.reverse()
     return route
 
 
-def _search_neighbor(map_: list, now: Node, open_list: list, close_list: list):
-    for x in range(-1, 2):
-        for y in range(-1, 2):
-            if now.POS[0] + x >= 0 and now.POS[1] + y >= 0 and map_[now.POS[0] + x][now.POS[1] + y] == 0:
-                t = Node(now.POS[0] + x, now.POS[1] + y, now)
-                if t not in open_list and t not in close_list and t != now:
-                    open_list.append(t)
-
-
-def _next_step(from_: Node, end_pos: Node, open_list: list, close_list: list) -> Node:
-    min_node = None
-    for node in open_list:
-        if node.is_near_by(from_):
-            g = from_.F + 1 if node.straight_to(from_) else from_.F + 1.4
-            h = _distance(node, end_pos)
-            f = g + h
-            if f <= node.F:
-                node.G = g
-                node.H = h
-                node.F = f
-                node.FROM = from_
-                min_node = node
-            else:
-                min_node = node
-    for node in open_list:
-        if node.FROM == from_ and node < min_node:
-            min_node = node
-    close_list.append(min_node)
-    open_list.remove(min_node)
-    # open_list.remove(from_)
+def lowest_f_in(open_list: list) -> Node:
+    min_node = open_list[0]
+    for n in open_list:
+        if n < min_node:
+            min_node = n
     return min_node
 
 
+def neighbor_of(x: Node, map_):
+    neighbor = []
+    for x_offset in range(-1, 2):
+        for y_offset in range(-1, 2):
+            try:
+                if map_[x.POS[0] + x_offset][x.POS[1] + y_offset] == 0:
+                    if not(x_offset == 0 and y_offset == 0):
+                        if x.POS[0] + x_offset >= 0 and x.POS[1] + y_offset >= 0:
+                            neighbor.append(Node(x.POS[0] + x_offset, x.POS[1] + y_offset))
+            except IndexError:
+                pass
+    return neighbor
+
+
 def _distance(a: Node, b: Node):
-    return round(math.sqrt((a.POS[0] - b.POS[0]) ** 2 + (a.POS[1] - b.POS[1]) ** 2), 5)
+    return math.sqrt((a.POS[0] - b.POS[0]) ** 2 + (a.POS[1] - b.POS[1]) ** 2)
+
+
+def _real_distance(a: Node, b: Node):
+    d = (a.POS[0] - b.POS[0]) ** 2 + (a.POS[1] - b.POS[1]) ** 2
+    if d >= 2:
+        return 1.4
+    else:
+        return 1
+
+
+def print_node_list(l: list):
+    for n in l:
+        print(n.POS, end=' ')
+    print()
 
 
 def main():
@@ -130,7 +161,7 @@ def main():
     start_pos = StartNode(0, 0)
     end_pos = Node(5, 2)
     route = a_star(start_pos, end_pos, exam_map)
-    print(route)
+    print_node_list(route)
 
 
 if __name__ == '__main__':
